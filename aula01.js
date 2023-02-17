fc225ef65649b4e0b34c35f6dce34fd1caba8c9b
Willames pereira de lima <Willames@academicos.com>
2023-02-16 21:24:36 -0300

    Aula JS

---
+++ aula01.js
@@ -0,0 +1,17 @@
+var aluno = "willames lima";
+var nota1 = 8.9;
+
+if (nota1 > 7){
+
+    console.log ("O aluno, willames, foi aprovado!");
+
+} else {
+
+    console.log ("O aluno, willames, foi reprovado!");
+
+}
+
+
+    
+
+
