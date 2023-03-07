b38d408a9a828521a8a325e2dd9331f6bb1cd9ac
WillamesLima01 <2022211510087@iesp.edu.br>
2023-03-06 22:45:53 -0300

    melhorias na aula03

--- Aula03.js
+++ Aula03.js
@@ -8,21 +8,21 @@ var menoraltura = 3.00;
 var resultado = 0;
 
 for (var i = 1; i <= 15; i++){
-    altura[i]= prompt("Informe sua altura:");    
-    sexo[i]= prompt("Informe seu sexo:");    
+    altura[i]= parseFloat(prompt("Informe sua altura: EX:1.70"));    
+    sexo[i]= prompt("Informe seu sexo: Ex: M, F");    
 }   
 
 for (var i = 1; i <= 15; i++){
     
     if (altura[i] > maioraltura){
-        maioraltura = altura[i];
+        maioraltura = (altura[i]).toFixed(2);
     } else {
        if (menoraltura > altura[i]) {
-            menoraltura = altura[i]
+            menoraltura = (altura[i]).toFixed(2);
        } 
     }
 
-    if (sexo[i] == "M" && sexo[i] == "m"){
+    if (sexo[i] == 'M'){
         soma = soma + altura[i];
         contador = contador + 1;
 
@@ -31,8 +31,8 @@ for (var i = 1; i <= 15; i++){
     }
 }
 
-resultado = soma/contador;
+resultado = (soma/contador).toFixed(2);
 
-alert(maioraltura);
-alert(menoraltura);
-alert(resultado)
+alert("A maior altura é : " + maioraltura + "m" + "\n" + "A menor altura é : " + menoraltura + "m" +
+        "\n" + "A Média de altura dos homens é : " + resultado + "m" + "\n" +
+        "O números de mulheres é (são) : " + qtdmulher);
