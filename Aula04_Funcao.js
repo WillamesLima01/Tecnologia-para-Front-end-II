cc04d995a1d02dbbedf49555912046f68b9d24a5
Willames pereira de lima <Willames@academicos.com>
2023-03-23 20:23:29 -0300

    aula04 23-03-23

---
+++ Aula04_Funcao.js
@@ -0,0 +1,14 @@
+function somar(numero1, numero2) {
+    var total = numero1 + numero2;
+    return total;
+}
+
+var resultado = somar(1,'5');
+
+console.log(resultado);
+
+var multiplicar = function(n1,n2) {
+    return(n1*n2);
+}
+
+console.log( multiplicar(2,4));
